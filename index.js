9036d3c551db59c33993ba4b36e39844ae9b4d67
bfbf <137762241+git23hubci@users.noreply.github.com>
2023-06-28 18:54:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+// const express = require('express');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// const app = express();
+// app.use('/', createProxyMiddleware({ target: 'https://github.com/', changeOrigin: true }));
+// app.listen(3000);
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-app.use('/', createProxyMiddleware({ target: 'https://github.com/', changeOrigin: true }));
+
+app.use('/', (req, res, next) => {
+  const { url } = req.query;
+  if (url) {
+    createProxyMiddleware({ target: url, changeOrigin: true })(req, res, next);
+  } else {
+    next();
+  }
+});
+
 app.listen(3000);
